06db7e9416a77bfe16c7ad9208c09d473569c69f
Shreedhar <urawane03@gmail.com>
2023-10-13 22:21:39 +0530

    create build

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    distDir: 'build',
+    experimental: {
+        serverActions: true,
+    },
 }
 const withPWA = require('next-pwa')
 
