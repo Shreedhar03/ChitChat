1e427bb6b3c935cf806efa433091b7a07cf82d20
Shreedhar <urawane03@gmail.com>
2023-10-13 22:40:44 +0530

    edit manifest

--- client/next.config.js
+++ client/next.config.js
@@ -1,19 +1,33 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    experimental: {
-        serverActions: true,
-    },
-}
-const withPWA = require('next-pwa')
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     experimental: {
+//         serverActions: true,
+//     },
+// }
+// const withPWA = require('next-pwa')
 
-module.exports = withPWA({
-    pwa: {
-        dest: "public",
-        register: true,
-        disable: process.env.NODE_ENV === 'development',
-        skipWaiting: true,
-    },
-})
+// module.exports = withPWA({
+//     pwa: {
+//         dest: "public",
+//         register: true,
+//         // disable: process.env.NODE_ENV === 'development',
+//         skipWaiting: true,
+//     },
+// })
 
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+const runtimeCaching = require("next-pwa/cache");
+const withPWA = require("next-pwa")({
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    runtimeCaching,
+    buildExcludes: [/middleware-manifest.json$/],
+});
+
+const nextConfig = withPWA({
+    // next config
+});
+module.exports = nextConfig;
