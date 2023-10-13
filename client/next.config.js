43136dc65146bc391775d67e209d9abafe7d1510
Shreedhar <urawane03@gmail.com>
2023-10-13 22:18:02 +0530

    remove expt. feature

--- client/next.config.js
+++ client/next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        serverActions: true,
-    },
     distDir: 'build',
 }
 const withPWA = require('next-pwa')
