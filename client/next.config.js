50f7150df830eda53502adda8f94df3d91068cb6
Shreedhar <urawane03@gmail.com>
2023-10-13 21:48:01 +0530

    make PWA compatible

--- client/next.config.js
+++ client/next.config.js
@@ -3,6 +3,18 @@ const nextConfig = {
     experimental: {
         serverActions: true,
     },
+    distDir: 'build',
 }
+const withPWA = require('next-pwa')
+
+module.exports = withPWA({
+    pwa: {
+        dest: "public",
+        register: true,
+        disable: process.env.NODE_ENV === 'development',
+        skipWaiting: true,
+    },
+})
+
 
 module.exports = nextConfig
